945286de7cdc12d1aada0ceb155f1f9fc55f5614
isa95330 <isabellemazecoiffure@gmail.com>
2024-08-31 20:01:31 +0200

    mise à jour des liens

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { HashLink } from "react-router-hash-link"; // Importez HashLink
 import Logo from "../Assets/logo-ud.jpg";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import Box from "@mui/material/Box";
@@ -17,39 +18,41 @@ import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuOptions = [
-    { text: "Home", icon: <HomeIcon /> },
-    { text: "About", icon: <InfoIcon /> },
-    { text: "Avis", icon: <CommentRoundedIcon /> },
-    { text: "Contact", icon: <PhoneRoundedIcon /> },
+    { text: "Accueil", icon: <HomeIcon />, path: "/#home" },
+    { text: "Pourquoi nous appeler", icon: <InfoIcon />, path: "/#why" },
+    { text: "Nos tarifs", icon: <CommentRoundedIcon />, path: "/#contact" },
+    { text: "Avis", icon: <CommentRoundedIcon />, path: "/#why" },
+    { text: "Contact", icon: <PhoneRoundedIcon />, path: "/#contact" },
   ];
 
   return (
     <nav>
       <div className="nav-top">
         <div className="nav-logo-container">
-          <img src={Logo} alt="Depanne-moi Logo" className="nav-logo" style={{ width: "120px" }} />
+          <HashLink to="/#home"> {/* Utilisez HashLink pour un défilement fluide */}
+            <img src={Logo} alt="Depanne-moi Logo" className="nav-logo" style={{ width: "120px" }} />
+          </HashLink>
         </div>
 
         <div className="nav-contact-container">
           <div className="nav-contact">
-          07.81.03.48.36
+            07.81.03.48.36
           </div>
         </div>
 
         <div className="nav-urgence-container">
           <div className="nav-urgence">
             Urgence dépannage 7j/7 24h/24
-
           </div>
         </div>
       </div>
 
       <div className="navbar-links-container">
-        <a href="#About">Accueil</a>
-        <a href="#why">Pourquoi nous appeler</a>
-        <a href="#why">Nos tarifs</a>
-        <a href="#About">Avis</a>
-        <a href="#contact">Contact</a>
+        {menuOptions.map((item) => (
+          <HashLink key={item.text} to={item.path}>
+            {item.text}
+          </HashLink>
+        ))}
       </div>
 
       <div className="navbar-menu-container">
@@ -66,7 +69,7 @@ const Navbar = () => {
           <List>
             {menuOptions.map((item) => (
               <ListItem key={item.text} disablePadding>
-                <ListItemButton>
+                <ListItemButton component={HashLink} to={item.path}>
                   <ListItemIcon>{item.icon}</ListItemIcon>
                   <ListItemText primary={item.text} />
                 </ListItemButton>
